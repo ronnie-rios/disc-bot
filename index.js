e642dd445b211782bfa35a0f6e6ce60efafdf185
Ronnie Rios <ronniesmail14@gmail.com>
2023-08-01 12:51:13 -0500

    client on functions for different message content

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('hi');
+const { channel } = require('diagnostics_channel');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+require('dotenv').config()
+
+client.on('ready',()=> {
+    console.log(`logged in as ${client.user.tag}`);
+})
+
+
+client.on("message", msg => {
+   // console.log(msg);
+    if(msg.content =='chicken') {
+        msg.reply('chicken run')
+    }
+});
+
+
+client.on('message', msg => {
+    if(msg.content.includes('!pin')) {
+        msg.reply('pinned message')
+    }
+})
+
+client.on('message', msg => {
+    if(msg.content.includes('!list')) {
+        
+    }
+})
+
+client.login(process.env.TOKEN)
